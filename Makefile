91da1f5aa3bb6381f9cb50589c1c7c743755198f
Dillon Huff <dillonhuff@gmail.com>
2017-12-29 11:26:20 -0700

    Creating real connect box

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+all: clb switch_box connect_box
+
 clb:
 	verilator -O3 -x-assign fast -noassert -Wall -Wno-DECLFILENAME --cc clb.v --exe clb_main.cpp --top-module clb -CFLAGS -O3 -CFLAGS -march=native
 	make OPT_FAST="-O3" -C obj_dir -j -f Vclb.mk Vclb
--- connect_box.v
+++ connect_box.v
@@ -1,4 +1,19 @@
-module connect_box();
+module connect_box(
+                   input  track0_in,
+                   input  track1_in,
+                   input  track2_in,
+                   input  track3_in,
 
+                   input  track0_out,
+                   input  track1_out,
+                   input  track2_out,
+                   input  track3_out,
+                   
+                   output block_out
+                   
+                   );
 
+   
+   
+   
 endmodule; // connect_box
