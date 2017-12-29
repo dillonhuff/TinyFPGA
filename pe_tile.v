71afc9d2a2820f59b03ecafa588d28ade0fe4f87
Dillon Huff <dillonhuff@gmail.com>
2017-12-29 12:34:48 -0700

    Filling in the fields of the PE tile

--- pe_tile.v
+++ pe_tile.v
@@ -1,3 +1,16 @@
-module pe_tile();
+module pe_tile(
+               
+               input                          clk,
+               input                          config_en,
+               // The address of the block being configured
+               input [7:0]                    config_addr,
+               // Config data: 2 bits for CLB, 2 bits for each CB, 16 bits for
+               // the switch block
+               input [(2 + 2 + 2 + 16 - 1):0] config_data);
+
+
+   parameter [7:0] address = 0;
+
+   
    
 endmodule
