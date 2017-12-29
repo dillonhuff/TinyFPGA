a42a08553bda340701916b5d6acb4f5b31716ac3
Dillon Huff <dillonhuff@gmail.com>
2017-12-29 12:23:15 -0700

    clb is now actually programmable

--- clb.v
+++ clb.v
@@ -1,11 +1,35 @@
 module clb(input in0,
            input  in1,
-           output and_out,
-           output or_out,
-           output xor_out);
+           output out,
+
+           input clk,
+           input config_enable,
+           input [1:0] config_data);
+
+   reg [1:0]           config_data_reg;
+
+   always @(posedge clk)
+     begin
+        if (config_enable)
+          begin
+             config_data_reg <= config_data;
+          end
+     end
+
+   wire                and_out, or_out, xor_out;
 
    assign and_out = in0 & in1;
    assign or_out = in0 | in1;
    assign xor_out = in0 ^ in1;
+
+   always @(*)
+     begin
+        case (config_data_reg)
+          2'd0: out = and_out;
+          2'd1: out = or_out;
+          2'd2: out = xor_out;
+          default: out = and_out;
+        endcase
+     end
    
 endmodule; // clb
--- clb_main.cpp
+++ clb_main.cpp
@@ -7,20 +7,112 @@
 
 using namespace std;
 
-int main(int argc, char** argv) {
+void test_and_out(int argc, char** argv) {
   Verilated::commandArgs(argc, argv);
   Vclb* top = new Vclb;
+
+  top->clk = 0;
+
+  top->eval();
+
+  top->clk = 1;
+  top->config_enable = 1;
+  top->config_data = 0;
+
+  top->eval();
+
+  top->config_enable = 0;
+  top->clk = 0;
 
   top->in0 = 1;
   top->in1 = 0;
 
   top->eval();
 
-  assert(top->and_out == 0);
-  assert(top->or_out == 1);
-  assert(top->xor_out == 1);
+  assert(top->out == 0);
+
+  top->in1 = 1;
+
+  top->eval();
+
+  assert(top->out == 1);
 
   top->final();
+}
+
+void test_or_out(int argc, char** argv) {
+  Verilated::commandArgs(argc, argv);
+  Vclb* top = new Vclb;
+
+  top->clk = 0;
+
+  top->eval();
+
+  top->clk = 1;
+  top->config_enable = 1;
+  top->config_data = 1;
+
+  top->eval();
+
+  top->config_enable = 0;
+  top->clk = 0;
+  
+  top->in0 = 1;
+  top->in1 = 0;
+
+  top->eval();
+
+  assert(top->out == 1);
+
+  top->in0 = 0;
+  top->in1 = 0;
+
+  top->eval();
+
+  assert(top->out == 0);
+
+  top->final();
+}
+
+void test_xor_out(int argc, char** argv) {
+  Verilated::commandArgs(argc, argv);
+  Vclb* top = new Vclb;
+
+  top->clk = 0;
+
+  top->eval();
+
+  top->clk = 1;
+  top->config_enable = 1;
+  top->config_data = 2;
+
+  top->eval();
+
+  top->config_enable = 0;
+  top->clk = 0;
+  
+  top->in0 = 1;
+  top->in1 = 0;
+
+  top->eval();
+
+  assert(top->out == 1);
+
+  top->in0 = 1;
+  top->in1 = 1;
+
+  top->eval();
+
+  assert(top->out == 0);
+
+  top->final();
+}
+
+int main(int argc, char** argv) {
+
+  test_and_out(argc, argv);
+  test_or_out(argc, argv);
+  test_xor_out(argc, argv);
 
   cout << "$$$$ clb tests pass" << endl;
 }
