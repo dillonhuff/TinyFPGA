5afa422b051707ce7435b3e4b23dfb5e23ee1f36
Dillon Huff <dillonhuff@gmail.com>
2017-12-30 00:54:09 -0700

    Creating tile wirings, definitely a bit of a mess

--- pe_tile.v
+++ pe_tile.v
@@ -3,6 +3,26 @@
 `include "switch_box.v"
 
 module pe_tile(
+
+                  output       left_0,
+                  input        left_1,
+                  output       left_2,
+                  input        left_3,
+
+                  input        right_0,
+                  output       right_1,
+                  input        right_2,
+                  output       right_3,
+
+                  input        top_0,
+                  output       top_1,
+                  input        top_2,
+                  output       top_3,
+                  
+                  output       bottom_0,
+                  input        bottom_1,
+                  output       bottom_2,
+                  input        bottom_3,
                
                input                          clk,
                input                          config_en,
@@ -19,18 +39,60 @@ module pe_tile(
 
    wire [1:0]                                       cb0_config;
    wire [1:0]                                       cb1_config;
+   wire [1:0]                                       clb_config;
+
+   wire [15:0]                                      sb_config;
+   
    
 
    assign configure_this_tile = config_addr == address ? 1 : 0;
 
    assign cb0_config = config_data[1:0];
    assign cb1_config = config_data[3:2];
+   assign clb_config = config_data[5:4];
+   assign sb_config = config_data[21:6];
+   
 
-   // clb compute_block();
+   
+   connect_box cb0();
+   connect_box cb1();
 
-   // connect_box cb0();
-   // connect_box cb1();
+   wire                                             compute_out;
+   
+   clb compute_block(cb0.block_out,
+                     cb1.block_out,
+                     compute_out,
+                     configure_this_tile,
+                     clb_config);
 
-   // switch_box sb
+   
+   switch_box sb(
+                 left_0,
+                 left_1,
+                 left_2,
+                 left_3,
+
+                 right_0,
+                 right_1,
+                 right_2,
+                 right_3,
+
+                 top_0,
+                 top_1,
+                 top_2,
+                 top_3,
+
+                 bottom_0,
+                 bottom_1,
+                 bottom_2,
+                 bottom_3,
+
+                 compute_out,
+
+                 sb_config,
+                 configure_this_tile,
+                 clk);
+   
+
    
 endmodule
