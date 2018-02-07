992ddbf98ccafde7018cc85fafc5a508a1c83454
Dillon Huff <dillonhuff@gmail.com>
2018-02-07 00:57:31 -0800

    The switch box proepr

--- gen_switch_box.py
+++ gen_switch_box.py
@@ -1,7 +1,20 @@
+
+n_sides = 4
+n_wires_per_side = 4
+
+# Generate the actual string
 mod_str = 'module switch_box(\n'
 
+for side_no in range(0, n_sides):
+    for wire_no in range(0, n_wires_per_side):
+        mod_str += '\tinput ' + 'in_wire_' + str(side_no) + '_' + str(wire_no) + ',\n'
+
+for side_no in range(0, n_sides):
+    for wire_no in range(0, n_wires_per_side):
+        mod_str += '\toutput ' + 'out_wire_' + str(side_no) + '_' + str(wire_no) + ',\n'
+        
 mod_str += '\tinput [31:0] config_data,\n'
-mod_str += '\tinput [31:0] config_addr,\n'
+mod_str += '\tinput config_en,\n'
 mod_str += '\tinput clk,\n'
 mod_str += '\tinput reset\n'
 mod_str += '\t);\n'
