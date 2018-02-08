15138f4da499d084bc5230f877554cb36200fcf0
Dillon Huff <dillonhuff@gmail.com>
2018-02-08 01:06:45 -0800

    Now generating the skeleton of the top module

--- gen_top.py
+++ gen_top.py
@@ -1,12 +1,43 @@
 from generator_utils import module_string
 
-def build_top_str():
-    includes = []
-    ports = []
+def build_top_str(num_in_ios,
+                  num_out_ios,
+                  grid_height,
+                  grid_width):
+
+    assert(num_in_ios <= grid_width);
+    assert(num_out_ios <= grid_width);
+
+    includes = ['pe_tile', 'io1in_pad', 'io1out_pad']
+    ports = ['input clk', 'input reset', 'input [31:0] config_addr', 'input [31:0] config_data']
+
+    for pad_no in range(0, num_in_ios):
+        ports.append('input in_wire_' + str(pad_no))
+
+    for pad_no in range(0, num_out_ios):
+        ports.append('output out_wire_' + str(pad_no))
+
+    # Need to initialize wire groups:
+    # 1. input io pads to tile grid
+    # 2. output io pads to top
+    # 3. wires between each pad on the grid
     body = ''
+    for pad_no in range(0, num_in_ios):
+        body += '\tio1in_pad in_pad_' + str(pad_no) + '(\n'
+        body += '\t\t.clk(clk),\n'
+        body += '\t\t.top_pin(in_wire_' + str(pad_no) + ')\n'
+        body += '\t);\n\n'
+
+
+    for pad_no in range(0, num_out_ios):
+        body += '\tio1out_pad out_pad_' + str(pad_no) + '(\n'
+        body += '\t\t.clk(clk),\n'
+        body += '\t\t.top_pin(out_wire_' + str(pad_no) + ')\n'
+        body += '\t);\n\n'
+        
     return module_string(includes, 'top', ports, body)
 
 pe_tile_file = open('top.v', 'w')
-pe_tile_file.write(build_top_str())
+pe_tile_file.write(build_top_str(16, 16, 16, 16))
 pe_tile_file.close()
     
--- top.v
+++ top.v
@@ -1,7 +1,205 @@
+`include "pe_tile"
+`include "io1in_pad"
+`include "io1out_pad"
 
 
 module top(
+	input clk,
+	input reset,
+	input [31:0] config_addr,
+	input [31:0] config_data,
+	input in_wire_0,
+	input in_wire_1,
+	input in_wire_2,
+	input in_wire_3,
+	input in_wire_4,
+	input in_wire_5,
+	input in_wire_6,
+	input in_wire_7,
+	input in_wire_8,
+	input in_wire_9,
+	input in_wire_10,
+	input in_wire_11,
+	input in_wire_12,
+	input in_wire_13,
+	input in_wire_14,
+	input in_wire_15,
+	output out_wire_0,
+	output out_wire_1,
+	output out_wire_2,
+	output out_wire_3,
+	output out_wire_4,
+	output out_wire_5,
+	output out_wire_6,
+	output out_wire_7,
+	output out_wire_8,
+	output out_wire_9,
+	output out_wire_10,
+	output out_wire_11,
+	output out_wire_12,
+	output out_wire_13,
+	output out_wire_14,
+	output out_wire_15
+	);
 
+	io1in_pad in_pad_0(
+		.clk(clk),
+		.top_pin(in_wire_0)
+	);
+
+	io1in_pad in_pad_1(
+		.clk(clk),
+		.top_pin(in_wire_1)
+	);
+
+	io1in_pad in_pad_2(
+		.clk(clk),
+		.top_pin(in_wire_2)
+	);
+
+	io1in_pad in_pad_3(
+		.clk(clk),
+		.top_pin(in_wire_3)
+	);
+
+	io1in_pad in_pad_4(
+		.clk(clk),
+		.top_pin(in_wire_4)
+	);
+
+	io1in_pad in_pad_5(
+		.clk(clk),
+		.top_pin(in_wire_5)
+	);
+
+	io1in_pad in_pad_6(
+		.clk(clk),
+		.top_pin(in_wire_6)
+	);
+
+	io1in_pad in_pad_7(
+		.clk(clk),
+		.top_pin(in_wire_7)
+	);
+
+	io1in_pad in_pad_8(
+		.clk(clk),
+		.top_pin(in_wire_8)
+	);
+
+	io1in_pad in_pad_9(
+		.clk(clk),
+		.top_pin(in_wire_9)
+	);
+
+	io1in_pad in_pad_10(
+		.clk(clk),
+		.top_pin(in_wire_10)
+	);
+
+	io1in_pad in_pad_11(
+		.clk(clk),
+		.top_pin(in_wire_11)
+	);
+
+	io1in_pad in_pad_12(
+		.clk(clk),
+		.top_pin(in_wire_12)
+	);
+
+	io1in_pad in_pad_13(
+		.clk(clk),
+		.top_pin(in_wire_13)
+	);
+
+	io1in_pad in_pad_14(
+		.clk(clk),
+		.top_pin(in_wire_14)
+	);
+
+	io1in_pad in_pad_15(
+		.clk(clk),
+		.top_pin(in_wire_15)
+	);
+
+	io1out_pad out_pad_0(
+		.clk(clk),
+		.top_pin(out_wire_0)
+	);
+
+	io1out_pad out_pad_1(
+		.clk(clk),
+		.top_pin(out_wire_1)
+	);
+
+	io1out_pad out_pad_2(
+		.clk(clk),
+		.top_pin(out_wire_2)
+	);
+
+	io1out_pad out_pad_3(
+		.clk(clk),
+		.top_pin(out_wire_3)
+	);
+
+	io1out_pad out_pad_4(
+		.clk(clk),
+		.top_pin(out_wire_4)
+	);
+
+	io1out_pad out_pad_5(
+		.clk(clk),
+		.top_pin(out_wire_5)
+	);
+
+	io1out_pad out_pad_6(
+		.clk(clk),
+		.top_pin(out_wire_6)
+	);
+
+	io1out_pad out_pad_7(
+		.clk(clk),
+		.top_pin(out_wire_7)
+	);
+
+	io1out_pad out_pad_8(
+		.clk(clk),
+		.top_pin(out_wire_8)
+	);
+
+	io1out_pad out_pad_9(
+		.clk(clk),
+		.top_pin(out_wire_9)
+	);
+
+	io1out_pad out_pad_10(
+		.clk(clk),
+		.top_pin(out_wire_10)
+	);
+
+	io1out_pad out_pad_11(
+		.clk(clk),
+		.top_pin(out_wire_11)
+	);
+
+	io1out_pad out_pad_12(
+		.clk(clk),
+		.top_pin(out_wire_12)
+	);
+
+	io1out_pad out_pad_13(
+		.clk(clk),
+		.top_pin(out_wire_13)
+	);
+
+	io1out_pad out_pad_14(
+		.clk(clk),
+		.top_pin(out_wire_14)
+	);
+
+	io1out_pad out_pad_15(
+		.clk(clk),
+		.top_pin(out_wire_15)
 	);
 
 
