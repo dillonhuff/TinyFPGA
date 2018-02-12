38547334962b33948c5e24185c2cadef76f91040
Dillon Huff <dillonhuff@gmail.com>
2018-02-12 00:07:03 -0800

    Creating parameters

--- run_generators.py
+++ run_generators.py
@@ -4,68 +4,36 @@ from gen_top import build_top_str
 
 
 def run_generators():
+    n_wires_per_side = 4
     # Center switch box
-    generate_sb('switch_box', [0, 1, 2, 3], 4, 4)
+    generate_sb('switch_box', [0, 1, 2, 3], 4, n_wires_per_side)
 
     # Middle side switch box
-    generate_sb('switch_box_top', [0, 1, 2], 4, 4)
-    generate_sb('switch_box_left', [0, 1, 3], 4, 4)
-    generate_sb('switch_box_right', [0, 1, 2, 3], 4, 4)
-    generate_sb('switch_box_bottom', [0, 1, 2, 3], 4, 4)
+    generate_sb('switch_box_top', [0, 1, 2], 4, n_wires_per_side)
+    generate_sb('switch_box_left', [0, 1, 3], 4, n_wires_per_side)
+    generate_sb('switch_box_right', [0, 1, 2, 3], 4, n_wires_per_side)
+    generate_sb('switch_box_bottom', [0, 1, 2, 3], 4, n_wires_per_side)
 
     # Corner switch boxes
-    generate_sb('switch_box_top_left', [0, 1], 4, 4)
-    generate_sb('switch_box_top_right', [0, 1, 2], 4, 4)
-    generate_sb('switch_box_bottom_left', [0, 1, 3], 4, 4)
-    generate_sb('switch_box_bottom_right', [0, 1, 2, 3], 4, 4)
+    generate_sb('switch_box_top_left', [0, 1], 4, n_wires_per_side)
+    generate_sb('switch_box_top_right', [0, 1, 2], 4, n_wires_per_side)
+    generate_sb('switch_box_bottom_left', [0, 1, 3], 4, n_wires_per_side)
+    generate_sb('switch_box_bottom_right', [0, 1, 2, 3], 4, n_wires_per_side)
 
     # Generating PE tiles
-    generate_pe_tile('pe_tile', 'switch_box', [0, 1, 2, 3], 4, 4)
-    # pe_tile_file = open('pe_tile.v', 'w')
-    # pe_tile_file.write(build_pe_tile_str('pe_tile', 'switch_box', [0, 1, 2, 3], 4, 4))
-    # pe_tile_file.close()
+    generate_pe_tile('pe_tile', 'switch_box', [0, 1, 2, 3], 4, n_wires_per_side)
 
     # Side PEs
-    generate_pe_tile('pe_tile_left', 'switch_box_left', [0, 1, 3], 4, 4)
-    # pe_tile_file = open('pe_tile_left.v', 'w')
-    # pe_tile_file.write(build_pe_tile_str('pe_tile_left', 'switch_box_left', [0, 1, 3], 4, 4))
-    # pe_tile_file.close()
-
-    generate_pe_tile('pe_tile_right', 'switch_box_right', [0, 1, 2, 3], 4, 4)
-    # pe_tile_file = open('pe_tile_right.v', 'w')
-    # pe_tile_file.write(build_pe_tile_str('pe_tile_right', 'switch_box_right', [1, 2, 3], 4, 4))
-    # pe_tile_file.close()
-
-    generate_pe_tile('pe_tile_top', 'switch_box_top', [0, 1, 2], 4, 4)
-    # pe_tile_file = open('pe_tile_top.v', 'w')
-    # pe_tile_file.write(build_pe_tile_str('pe_tile_top', 'switch_box_top', [0, 1, 2], 4, 4))
-    # pe_tile_file.close()
-
-    generate_pe_tile('pe_tile_bottom', 'switch_box_bottom', [0, 2, 3], 4, 4)
-    # pe_tile_file = open('pe_tile_bottom.v', 'w')
-    # pe_tile_file.write(build_pe_tile_str('pe_tile_bottom', 'switch_box_bottom', [0, 2, 3], 4, 4))
-    # pe_tile_file.close()
+    generate_pe_tile('pe_tile_left', 'switch_box_left', [0, 1, 3], 4, n_wires_per_side)
+    generate_pe_tile('pe_tile_right', 'switch_box_right', [0, 1, 2, 3], 4, n_wires_per_side)
+    generate_pe_tile('pe_tile_top', 'switch_box_top', [0, 1, 2], 4, n_wires_per_side)
+    generate_pe_tile('pe_tile_bottom', 'switch_box_bottom', [0, 2, 3], 4, n_wires_per_side)
 
     # Corner tiles
-    generate_pe_tile('pe_tile_top_left', 'switch_box_top_left', [0, 1], 4, 4)
-    # pe_tile_file = open('pe_tile_top_left.v', 'w')
-    # pe_tile_file.write(build_pe_tile_str('pe_tile_top_left', 'switch_box_top_left', [0, 1], 4, 4))
-    # pe_tile_file.close()
-
-    generate_pe_tile('pe_tile_top_right', 'switch_box_top_right', [1, 2], 4, 4)
-    # pe_tile_file = open('pe_tile_top_right.v', 'w')
-    # pe_tile_file.write(build_pe_tile_str('pe_tile_top_right', 'switch_box_top_right', [1, 2], 4, 4))
-    # pe_tile_file.close()
-
-    generate_pe_tile('pe_tile_bottom_left', 'switch_box_bottom_left', [0, 3], 4, 4)
-    # pe_tile_file = open('pe_tile_bottom_left.v', 'w')
-    # pe_tile_file.write(build_pe_tile_str('pe_tile_bottom_left', 'switch_box_bottom_left', [0, 3], 4, 4))
-    # pe_tile_file.close()
-
-    generate_pe_tile('pe_tile_bottom_right', 'switch_box_bottom_right', [2, 3], 4, 4)
-    # pe_tile_file = open('pe_tile_bottom_right.v', 'w')
-    # pe_tile_file.write(build_pe_tile_str('pe_tile_bottom_right', 'switch_box_bottom_right', [2, 3], 4, 4))
-    # pe_tile_file.close()
+    generate_pe_tile('pe_tile_top_left', 'switch_box_top_left', [0, 1], 4, n_wires_per_side)
+    generate_pe_tile('pe_tile_top_right', 'switch_box_top_right', [1, 2], 4, n_wires_per_side)
+    generate_pe_tile('pe_tile_bottom_left', 'switch_box_bottom_left', [0, 3], 4, n_wires_per_side)
+    generate_pe_tile('pe_tile_bottom_right', 'switch_box_bottom_right', [2, 3], 4, n_wires_per_side)
 
     name = 'top'
     pe_tile_file = open(name + '.v', 'w')
