c4ead67367447d8643153b2a868efd3e3738a7ab
Dillon Huff <dillonhuff@gmail.com>
2018-02-26 16:32:13 -0800

    Need to load the actual application graph from json

--- place_and_route/src/pnr_main.cpp
+++ place_and_route/src/pnr_main.cpp
@@ -1,17 +1,23 @@
 #include <cassert>
 
 #include "bitstream_format.h"
+#include "place.h"
 
 using namespace std;
 using namespace TinyPnR;
 
+ApplicationGraph loadApplicationFromJSON(const std::string& applicationFile) {
+  assert(false);
+}
+
 int main(const int argc, const char** argv) {
   assert(argc == 5);
 
-  string app_file = argv[1];
-  string bitstream_format_file = argv[2];
-  string topology_file = argv[3];
-  string application_file = argv[4];
+  string appFile = argv[1];
+  string bitstreamFormatFile = argv[2];
+  string topologyFile = argv[3];
+  string applicationFile = argv[4];
 
+  ApplicationGraph app = loadApplicationFromJSON(applicationFile);
   
 }
