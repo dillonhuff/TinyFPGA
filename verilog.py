a86f76fd81659ed7dd9b8a0296b65510584326e7
Dillon Huff <dillonhuff@gmail.com>
2018-03-20 20:18:16 -0700

    Maybe now is the time to create a proper verilog module class

--- verilog.py
+++ verilog.py
@@ -39,8 +39,8 @@ class VerilogModule():
         self.internal_wires.add(wire_name)
         self.wire_widths[wire_name] = width
 
-    def add_instance(self, mod_name, inst_name, parameters=()):
-        self.instances.add((mod_name, inst_name, parameters))
+    def add_instance(self, mod_name, inst_name, parameters):
+        self.instances.add((mod_name, inst_name))
         self.inst_to_wires[inst_name] = []
 
     def add_assign(self, in_wire, driver_value):
@@ -89,14 +89,8 @@ class VerilogModule():
         for inst in self.instances:
             mod_name = inst[0]
             inst_name = inst[1]
-            parameters = inst[2]
 
             body += '\t' + mod_name
-            if (len(parameters) > 0):
-                body += '# ('
-                for param in parameters:
-                    body += 'parameter ' + str(param)
-                body += ')'
             body += ' ' + inst_name + '(\n'
 
             print 'inst = ', inst_name
