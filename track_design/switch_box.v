b4f1d60f6ef0404bed18597e99632a1275e2acae
Dillon Huff <dillonhuff@gmail.com>
2018-06-10 21:23:16 -0700

    Creating switch box

---
+++ track_design/switch_box.v
@@ -0,0 +1,37 @@
+
+//                2
+//
+// 
+//  3        switch_box        1
+//
+//                
+//                0
+
+module switch_box(input clk,
+                  input rst,
+
+                  // Vertical tracks
+                  input side_0_track_0_in,
+                  input side_0_track_1_in,
+                  input side_0_track_2_in,
+                  input side_0_track_3_in,
+
+                  input side_1_track_0_in,
+                  input side_1_track_1_in,
+                  input side_1_track_2_in,
+                  input side_1_track_3_in,
+
+                  input side_2_track_0_in,
+                  input side_2_track_1_in,
+                  input side_2_track_2_in,
+                  input side_2_track_3_in,
+
+                  input side_3_track_0_in,
+                  input side_3_track_1_in,
+                  input side_3_track_2_in,
+                  input side_3_track_3_in,
+
+                  );
+
+   
+endmodule
