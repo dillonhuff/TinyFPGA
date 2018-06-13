7c342751c304f041cd77d3bd455437faaa241320
Dillon Huff <dillonhuff@gmail.com>
2018-06-13 01:03:24 -0700

    Creating switch configuration

--- track_design/switch_box.v
+++ track_design/switch_box.v
@@ -13,49 +13,41 @@ module switch_box(input clk,
                   input  config_en,
                   input  [31:0] config_data,
 
-                  // Vertical tracks
                   input  side_0_track_0_in,
                   input  side_0_track_1_in,
                   input  side_0_track_2_in,
                   input  side_0_track_3_in,
 
-                  // Horizontal tracks
                   input  side_1_track_0_in,
                   input  side_1_track_1_in,
                   input  side_1_track_2_in,
                   input  side_1_track_3_in,
 
-                  // Vertical tracks
                   input  side_2_track_0_in,
                   input  side_2_track_1_in,
                   input  side_2_track_2_in,
                   input  side_2_track_3_in,
 
-                  // Horizontal tracks
                   input  side_3_track_0_in,
                   input  side_3_track_1_in,
                   input  side_3_track_2_in,
                   input  side_3_track_3_in,
 
-                  // Vertical tracks
                   output side_0_track_0_out,
                   output side_0_track_1_out,
                   output side_0_track_2_out,
                   output side_0_track_3_out,
 
-                  // Horizontal tracks
                   output side_1_track_0_out,
                   output side_1_track_1_out,
                   output side_1_track_2_out,
                   output side_1_track_3_out,
 
-                  // Vertical tracks
                   output side_2_track_0_out,
                   output side_2_track_1_out,
                   output side_2_track_2_out,
                   output side_2_track_3_out,
 
-                  // Horizontal tracks
                   output side_3_track_0_out,
                   output side_3_track_1_out,
                   output side_3_track_2_out,
@@ -75,6 +67,7 @@ module switch_box(input clk,
       end
    end
 
+   // Side 0
    always @(*) begin
       case (config_data_reg[1:0])
         2'b00: side_0_track_0_out = side_0_track_0_in;
@@ -85,7 +78,7 @@ module switch_box(input clk,
    end
 
    always @(*) begin
-      case (config_data_reg[1:0])
+      case (config_data_reg[3:2])
         2'b00: side_0_track_1_out = side_0_track_1_in;
         2'b01: side_0_track_1_out = side_1_track_1_in;
         2'b10: side_0_track_1_out = side_2_track_1_in;
@@ -94,7 +87,7 @@ module switch_box(input clk,
    end
 
    always @(*) begin
-      case (config_data_reg[1:0])
+      case (config_data_reg[5:4])
         2'b00: side_0_track_2_out = side_0_track_2_in;
         2'b01: side_0_track_2_out = side_1_track_2_in;
         2'b10: side_0_track_2_out = side_2_track_2_in;
@@ -103,11 +96,122 @@ module switch_box(input clk,
    end
 
    always @(*) begin
-      case (config_data_reg[1:0])
+      case (config_data_reg[7:6])
         2'b00: side_0_track_3_out = side_0_track_3_in;
         2'b01: side_0_track_3_out = side_1_track_3_in;
         2'b10: side_0_track_3_out = side_2_track_3_in;
         2'b11: side_0_track_3_out = side_3_track_3_in;
+      endcase
+   end
+
+   // Side 1
+   always @(*) begin
+      case (config_data_reg[1:0])
+        2'b00: side_1_track_0_out = side_0_track_0_in;
+        2'b01: side_1_track_0_out = side_1_track_0_in;
+        2'b10: side_1_track_0_out = side_2_track_0_in;
+        2'b11: side_1_track_0_out = side_3_track_0_in;
+      endcase
+   end
+
+   always @(*) begin
+      case (config_data_reg[1:0])
+        2'b00: side_1_track_1_out = side_0_track_1_in;
+        2'b01: side_1_track_1_out = side_1_track_1_in;
+        2'b10: side_1_track_1_out = side_2_track_1_in;
+        2'b11: side_1_track_1_out = side_3_track_1_in;
+      endcase
+   end
+
+   always @(*) begin
+      case (config_data_reg[1:0])
+        2'b00: side_1_track_2_out = side_0_track_2_in;
+        2'b01: side_1_track_2_out = side_1_track_2_in;
+        2'b10: side_1_track_2_out = side_2_track_2_in;
+        2'b11: side_1_track_2_out = side_3_track_2_in;
+      endcase
+   end
+
+   always @(*) begin
+      case (config_data_reg[1:0])
+        2'b00: side_1_track_3_out = side_0_track_3_in;
+        2'b01: side_1_track_3_out = side_1_track_3_in;
+        2'b10: side_1_track_3_out = side_2_track_3_in;
+        2'b11: side_1_track_3_out = side_3_track_3_in;
+      endcase
+   end
+
+   // Side 2
+   always @(*) begin
+      case (config_data_reg[1:0])
+        2'b00: side_2_track_0_out = side_0_track_0_in;
+        2'b01: side_2_track_0_out = side_1_track_0_in;
+        2'b10: side_2_track_0_out = side_2_track_0_in;
+        2'b11: side_2_track_0_out = side_3_track_0_in;
+      endcase
+   end
+
+   always @(*) begin
+      case (config_data_reg[1:0])
+        2'b00: side_2_track_1_out = side_0_track_1_in;
+        2'b01: side_2_track_1_out = side_1_track_1_in;
+        2'b10: side_2_track_1_out = side_2_track_1_in;
+        2'b11: side_2_track_1_out = side_3_track_1_in;
+      endcase
+   end
+
+   always @(*) begin
+      case (config_data_reg[1:0])
+        2'b00: side_2_track_2_out = side_0_track_2_in;
+        2'b01: side_2_track_2_out = side_1_track_2_in;
+        2'b10: side_2_track_2_out = side_2_track_2_in;
+        2'b11: side_2_track_2_out = side_3_track_2_in;
+      endcase
+   end
+
+   always @(*) begin
+      case (config_data_reg[1:0])
+        2'b00: side_2_track_3_out = side_0_track_3_in;
+        2'b01: side_2_track_3_out = side_1_track_3_in;
+        2'b10: side_2_track_3_out = side_2_track_3_in;
+        2'b11: side_2_track_3_out = side_3_track_3_in;
+      endcase
+   end
+
+   // Side 3
+   always @(*) begin
+      case (config_data_reg[1:0])
+        2'b00: side_3_track_0_out = side_0_track_0_in;
+        2'b01: side_3_track_0_out = side_1_track_0_in;
+        2'b10: side_3_track_0_out = side_2_track_0_in;
+        2'b11: side_3_track_0_out = side_3_track_0_in;
+      endcase
+   end
+
+   always @(*) begin
+      case (config_data_reg[1:0])
+        2'b00: side_3_track_1_out = side_0_track_1_in;
+        2'b01: side_3_track_1_out = side_1_track_1_in;
+        2'b10: side_3_track_1_out = side_2_track_1_in;
+        2'b11: side_3_track_1_out = side_3_track_1_in;
+      endcase
+   end
+
+   always @(*) begin
+      case (config_data_reg[1:0])
+        2'b00: side_3_track_2_out = side_0_track_2_in;
+        2'b01: side_3_track_2_out = side_1_track_2_in;
+        2'b10: side_3_track_2_out = side_2_track_2_in;
+        2'b11: side_3_track_2_out = side_3_track_2_in;
+      endcase
+   end
+
+   always @(*) begin
+      case (config_data_reg[1:0])
+        2'b00: side_3_track_3_out = side_0_track_3_in;
+        2'b01: side_3_track_3_out = side_1_track_3_in;
+        2'b10: side_3_track_3_out = side_2_track_3_in;
+        2'b11: side_3_track_3_out = side_3_track_3_in;
       endcase
    end
    
