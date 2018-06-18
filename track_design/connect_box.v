3bb1a54944a0533754878a20aa227038406ea3ae
Dillon Huff <dillonhuff@gmail.com>
2018-06-17 19:06:59 -0700

    Added pe tile configuration

--- track_design/connect_box.v
+++ track_design/connect_box.v
@@ -1,5 +1,47 @@
 module connect_box(input clk,
-                   input rst);
+                   input rst,
 
+                   input config_en,
+                   input [31:0] config_data,
+
+                   input track_0_in,
+                   input track_1_in,
+                   input track_2_in,
+                   input track_3_in,
+
+                   input track_0_out,
+                   input track_1_out,
+                   input track_2_out,
+                   input track_3_out,
+
+                   output out
+                   );
+
+   reg [31:0]            config_data_reg;
+   always @(posedge clk or negedge rst) begin
+      if (~rst) begin
+         config_data_reg <= 32'h0;
+      end else if (config_en) begin
+         config_data_reg <= config_data;
+      end
+   end
+
+   reg output_wire_reg;
+   
+   always @(*) begin
+      case (config_data_reg)
+        32'd0: output_wire_reg = track_0_in;
+        32'd1: output_wire_reg = track_1_in;
+        32'd2: output_wire_reg = track_2_in;
+        32'd3: output_wire_reg = track_3_in;
+
+        32'd0: output_wire_reg = track_0_out;
+        32'd1: output_wire_reg = track_1_out;
+        32'd2: output_wire_reg = track_2_out;
+        32'd3: output_wire_reg = track_3_out;
+      endcase
+   end
+
+   assign out = output_wire_reg;
    
 endmodule
--- track_design/pe_tile.v
+++ track_design/pe_tile.v
@@ -45,7 +45,64 @@ module pe_tile(input clk,
                output       side_3_track_1_out,
                output       side_3_track_2_out,
                output       side_3_track_3_out
-                  );
+               );
 
+   wire                     operand0;
+   wire                     operand1;
+
+   wire                     config_this_tile;
+   
+   wire                     config_en_cb0;
+   wire                     config_en_cb1;
+   wire                     config_en_sb;
+   
+
+   assign config_this_tile = config_addr[15:0] == tile_id;
+   
+   assign config_en_cb0 = config_this_tile && (config_addr[31:16] == 4);
+   assign config_en_cb1 = config_this_tile && (config_addr[31:16] == 5);
+   assign config_en_sb = config_this_tile && (config_addr[31:16] == 6);
+   
+   
+   connect_box cb0(.clk(clk),
+                   .rst(rst),
+                   .config_en(config_en_cb0),
+                   .config_data(config_data),
+                   
+                   .track_0_in(side_0_track_0_in),
+                   .track_1_in(side_0_track_1_in),
+                   .track_2_in(side_0_track_2_in),
+                   .track_3_in(side_0_track_3_in),
+
+                   .track_0_out(side_0_track_0_out),
+                   .track_1_out(side_0_track_1_out),
+                   .track_2_out(side_0_track_2_out),
+                   .track_3_out(side_0_track_3_out),
+
+                   .out(operand0));
+
+   connect_box cb1(.clk(clk),
+                   .rst(rst),
+                   .config_en(config_en_cb1),
+                   .config_data(config_data),
+                   
+                   .track_0_in(side_1_track_0_in),
+                   .track_1_in(side_1_track_1_in),
+                   .track_2_in(side_1_track_2_in),
+                   .track_3_in(side_1_track_3_in),
+
+                   .track_0_out(side_1_track_0_out),
+                   .track_1_out(side_1_track_1_out),
+                   .track_2_out(side_1_track_2_out),
+                   .track_3_out(side_1_track_3_out),
+
+                   .out(operand1));
+
+   switch_box sb(.clk(clk),
+                 .rst(rst),
+
+                 .config_en(config_en_sb),
+                 .config_data(config_data));
+   
    
 endmodule
